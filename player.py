9ed077f5d3321e2779f6df6d5f907a6b06fbc106
yuwe <25470828+yuwex@users.noreply.github.com>
2021-03-08 11:42:38 -0800

    added id

--- player.py
+++ player.py
@@ -2,10 +2,11 @@ from robot import Robot
 from part import Part
 
 class Player:
-    def __init__(self, name='empty', gold=0):
+    def __init__(self, name='empty', gold=0, identity=0):
         self.name = name
         self.gold = gold
         self.robot = Robot()
+        self.identity = identity
 
     def change_gold(self, gold=2):
         self.gold += gold

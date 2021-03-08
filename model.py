a7b38dc5e5c66d4dea9fee1e0e02d2f8c14b8adc
yuwe <25470828+yuwex@users.noreply.github.com>
2021-03-08 11:42:27 -0800

    added turns

--- model.py
+++ model.py
@@ -11,11 +11,18 @@ class Model:
         self.players = []
         self.deck = Deck()
         self.scrapyard = Scrapyard(self.deck.take_card(4))
+        self.current_turn = None
 
     def create_players(self, player_names, player_golds):
         if len(player_names) == len(player_golds):
             for i in range(len(player_names)):
-                self.players.append(Player(name = player_names[i], gold = player_golds[i]))
+                self.players.append(Player(name = player_names[i], gold = player_golds[i], identity=i))
+            self.current_turn = 0
         else:
             print('error')
     
+    def increment_turn(self):
+        self.current_turn = (self.current_turn + 1) % len(self.players)
+    
+    def get_turn(self):
+        return self.current_turn
